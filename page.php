c68714c3b02996b79f9bf468467b3452db759e31
BluePraise <magaliechetrit@gmail.com>
2014-07-08 14:07:23 +0200

    added small logo and pages now can show content. edit link is now in dutch

--- page.php
+++ page.php
@@ -12,15 +12,14 @@ get_sidebar( );
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
         the_content();
-        endwhile; ?>
-<?php else: ?>
-<!-- no posts found -->
-<span>Binnenkort wordt deze pagina gevuldt, we zijn er erg druk mee.</span>
-<?php endif;
-
-if ( ! post_password_required()) :
-  edit_post_link( __( 'Edit', '' ), '<span class="edit-link">', '</span>' );
-endif;
+        endwhile;
+        else: ?>
+        <!-- no posts found -->
+        <span>Binnenkort wordt deze pagina gevuldt, we zijn er erg druk mee.</span>
+    <?php endif;
+          if ( ! post_password_required()) :
+          edit_post_link( __( 'Aanpassen', '' ), '<span class="edit-link">', '</span>' );
+          endif; ?>
 
 ?>
 </div> <!-- end of contentclass -->
